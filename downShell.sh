b233c0df58b6e27a946f6f57ba02d5ec0c7be2f2
unknown <jintao2@senseauto.com>
2023-11-08 21:16:47 +0800

    添加shell脚本

---
+++ downShell.sh
@@ -0,0 +1,1 @@
+aws --endpoint-url=http://sdc-oss.ceph.sensebee.xyz --profile ad_system_common s3 cp s3://sdc_hozon/HOZON/segment/feature/18557/2023_10/0S-54/2023-10-29/2023_10_29_19_10_10/simulator_scenario/2023_10_29_19_10_10.zip /data/2023_10_29_19_10_10.zip
